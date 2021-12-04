5010eecde1200d1abc32296cd7516405423d6c90
INTELLISOFT <admnetmail@gmai.com>
2021-12-05 02:25:29 +0500

    added carouselvew in homefragment

--- app/src/main/java/uz/intellisoft/hosman/screen/home/HomeFragment.kt
+++ app/src/main/java/uz/intellisoft/hosman/screen/home/HomeFragment.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_home.*
 import uz.intellisoft.hosman.R
 class HomeFragment : Fragment() {
+
+    var images = arrayOf(R.drawable.img1,R.drawable.img2,R.drawable.img3,)
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -15,6 +19,14 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        carouselView.pageCount=images.count()
+        carouselView.setImageListener { position, imageView ->
+            imageView.setImageResource(images[position])
+        }
+    }
     companion object {
 
         @JvmStatic
