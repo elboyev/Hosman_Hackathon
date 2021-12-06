43e30f58c469c2bbb947a69f99b3fb840e7119e7
INTELLISOFT <admnetmail@gmai.com>
2021-12-07 00:42:59 +0500

    deleted map activity and fix error

--- app/src/main/java/uz/intellisoft/hosman/screen/search/SearchFragment.kt
+++ app/src/main/java/uz/intellisoft/hosman/screen/search/SearchFragment.kt
@@ -10,7 +10,7 @@ import uz.intellisoft.hosman.R
 class SearchFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_maps, container, false)
+        return inflater.inflate(R.layout.fragment_search, container, false)
     }
 
     companion object {
